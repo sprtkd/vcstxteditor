9727449dfdacb071a908535cc6d086b7110dbff3
Suprotik Dey <supdey1@gmail.com>
2019-04-07 23:11:46 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { VcsEditorComponent } from './vcs-editor/vcs-editor.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VcsEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    NgxEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
